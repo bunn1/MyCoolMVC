253694f9ead59e7c0ce093edbed9c41c82f2a6e9
unknown <anders.sjunnesson@gmail.com>
2022-05-02 12:00:08 +0200

    Express Hello World

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-console.log("Allå allå");
+// dependencies
+import express from 'express';
+
+// "app" environment
+const app = express();
+
+// variables
+const port = 3000;
+
+// listen to requests
+app.get('/', (req, res) => {
+    res.send('Hello World')
+});
+
+// start server
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
